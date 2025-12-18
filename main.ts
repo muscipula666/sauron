f967b5300b78ebe45f195f4e4550b8a77cf60e42
Andrew Sprott <andrew.sprott@gmail.com>
2025-12-18 13:20:36 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-kitronik_VIEW128x64.controlDisplayOnOff(kitronik_VIEW128x64.onOff(true))
-kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Normal)
-kitronik_VIEW128x64.drawRect(60, 30, 0, 0)
 basic.forever(function () {
 	
 })
