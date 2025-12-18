c290fdb7f2f6a48fd93940a1fee2bc7e39ba92d7
Andrew Sprott <andrew.sprott@gmail.com>
2025-12-18 11:29:51 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+kitronik_VIEW128x64.controlDisplayOnOff(kitronik_VIEW128x64.onOff(true))
+kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Normal)
+kitronik_VIEW128x64.drawRect(60, 30, 0, 0)
+basic.forever(function () {
+	
+})
